2ef0cd6df628eb09b01c9e7d7d16d95fa1759c67
zhu <123456@123.com>
2019-10-14 16:37:29 +0800

    修改了test

---
+++ app_api/src/main/java/com/anjian/jiuxiao/util/Test.java
@@ -0,0 +1,14 @@
+package com.anjian.jiuxiao.util;
+
+/**
+ * @author ：努力吧小朱.
+ * @version : Java1.8
+ * @date ：Created in 16:36 2019/10/14
+ * @ Description：${description}
+ * @ Modified By：
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(123);
+    }
+}
